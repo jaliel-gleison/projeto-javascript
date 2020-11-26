3e33072fa7a4a3dc5f44526c568108bb2f052362
Jaliel Gleison <jalielgleison@hotmail.com>
2020-11-26 01:13:42 -0300

    trabalhando com javascript

--- js/main.js
+++ js/main.js
@@ -1,3 +1,55 @@
+function funcaochange(elemento){
+    console.log(elemento.value);
+}
+
+function load(){
+    alert("pagina carregado com sucesso");
+}
+
+function trocar(elemento){
+    elemento.innerHTML = "Obrigado por passar o mouse";
+    //document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse";
+}
+
+function voltar(elemento){
+    elemento.innerHTML = "Passe o mouse aqui";
+    //document.getElementById("mousemove").innerHTML = "Passe o mouse aqui";
+}
+
+/*function redirecionamento(){
+    window.open("https://www.youtube.com/watch?v=kYWlN2UbGFA");
+}*/
+
+function clicou(){
+    document.getElementById("agradecimento").innerHTML = "Obrigado por Clicar";
+    //console.log(document.getElementById("agradecimento"));
+    //alert("Obrigado por clicar ");
+
+}
+
+/*function validarIdade(idade){
+    var validar;
+    if (idade >= 18){
+        validar= true
+    }else{
+        validar= false               
+    }
+    return validar;
+}
+var idade = prompt("Qual  sua idade?");
+console.log(validarIdade(idade));
+
+function setReplace(frase, nome, novo_nome){
+    return frase.replace(nome, novo_nome);
+    }
+    alert(setReplace("Vai Japão", "Japão", "Brasil"));
+
+
+function soma(n1,n2){
+    return n1+n2;
+}
+alert(soma(5,10));
+
 var d = new Date();
 alert(d);
 
@@ -48,4 +100,4 @@ var frase = "Japão é o melhor time do mundo";
 //alert(idade + idade2);
 console.log(n1 + n2);
 console.log(frase.toUpperCase());
-alert(frase.replace("Japão","Braseil"));
+alert(frase.replace("Japão","Braseil"));*/
